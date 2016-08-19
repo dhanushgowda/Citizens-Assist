123f06be1e54bc8471fed6a4d9080b04fcb1001f
Swathi Ashok <swathia@thoughtworks.com>
2016-08-19 11:52:33 +0530

    Swathi | Removes login and register at current stage

--- app/src/main/java/com/tw/awayday/citizensassist/MainActivity.java
+++ app/src/main/java/com/tw/awayday/citizensassist/MainActivity.java
@@ -5,24 +5,12 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 
-import com.google.gson.JsonObject;
-import com.koushikdutta.async.future.FutureCallback;
-
 import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
-import static android.widget.Toast.LENGTH_LONG;
 import static android.widget.Toast.LENGTH_SHORT;
 import static android.widget.Toast.makeText;
-import static com.koushikdutta.ion.Ion.with;
 import static com.tw.awayday.citizensassist.Constants.REQUEST_IMAGE_CAPTURE;
-import static com.tw.awayday.citizensassist.ServerDetails.SERVER_URL;
-import static com.tw.awayday.citizensassist.ServerDetails.validateUser;
-import static com.tw.awayday.citizensassist.UserCredentials.currentUser;
-import static com.tw.awayday.citizensassist.UserCredentials.loggedIn;
-import static com.tw.awayday.citizensassist.UserMessages.ERROR_TRY_AGAIN;
-import static com.tw.awayday.citizensassist.UserMessages.LOGIN_FAILED;
 import static com.tw.awayday.citizensassist.UserMessages.OPENING_CAMERA;
 import static com.tw.awayday.citizensassist.UserMessages.WORK_UNDER_PROGRESS;
 
@@ -32,24 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!loggedIn) {
-            login();
-        } else {
-            home();
-        }
-    }
-
-    private void login() {
-        setContentView(R.layout.login_page);
-
-        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText email = (EditText) findViewById(R.id.email);
-                EditText password = (EditText) findViewById(R.id.pword);
-                validateUser(email.getText().toString(), password.getText().toString());
-            }
-        });
+        home();
     }
 
     private void home() {
@@ -76,31 +47,6 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-    }
-
-    private void validateUser(final String userEmail, final String userPassword) {
-        with(this)
-                .load(SERVER_URL + validateUser)
-                .setJsonPojoBody(new User(userEmail, userPassword))
-                .asJsonObject()
-                .setCallback(new FutureCallback<JsonObject>() {
-                    @Override
-                    public void onCompleted(Exception e, JsonObject result) {
-                        if (e != null) {
-                            makeText(getApplicationContext(), ERROR_TRY_AGAIN, LENGTH_LONG).show();
-                            login();
-                        }
-
-                        if (result.get("message").getAsString().equals("Failed")) {
-                            makeText(getApplicationContext(), LOGIN_FAILED, LENGTH_LONG).show();
-                            login();
-                        }
-
-                        loggedIn = true;
-                        currentUser.is(userEmail, userPassword);
-                        home();
-                    }
-                });
     }
 
     private void dispatchTakePictureIntent() {
