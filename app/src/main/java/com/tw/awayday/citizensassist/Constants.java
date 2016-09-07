9cb4d774a7f7b4cf169c74fafe2c82e24798afe0
nayanab <nayanabm.94@gmail.com>
2016-09-07 16:36:57 +0530

    Nayana | raise isuue icon

--- app/src/main/java/com/tw/awayday/citizensassist/Constants.java
+++ app/src/main/java/com/tw/awayday/citizensassist/Constants.java
@@ -2,6 +2,4 @@ package com.tw.awayday.citizensassist;
 
 public class Constants {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int TAG_LOCATION = 2;
-
 }
--- app/src/main/java/com/tw/awayday/citizensassist/UserMessages.java
+++ app/src/main/java/com/tw/awayday/citizensassist/UserMessages.java
@@ -2,6 +2,6 @@ package com.tw.awayday.citizensassist;
 
 public class UserMessages {
     public static final String OPENING_CAMERA = "Opening Camera";
-    public static final String OPENING_MAPS = "Opening maps";
+    public static final String OPENING_MAPS = "Opening Maps";
     public static final String IMAGE = "image";
 }
