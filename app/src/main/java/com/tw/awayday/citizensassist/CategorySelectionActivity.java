76acdc71ef8fedc5739b0a5e2757f9e97a48fb42
Swathi Ashok <swathia@thoughtworks.com>
2016-09-14 17:26:30 +0530

    Swathi | Something I did becuase alge is annoying

--- app/src/main/java/com/tw/awayday/citizensassist/CategorySelectionActivity.java
+++ app/src/main/java/com/tw/awayday/citizensassist/CategorySelectionActivity.java
@@ -5,11 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import static android.widget.ArrayAdapter.createFromResource;
 import static android.widget.Toast.LENGTH_SHORT;
 import static android.widget.Toast.makeText;
 import static com.tw.awayday.citizensassist.MainActivity.newIssue;
-import static com.tw.awayday.citizensassist.UserMessages.*;
+import static com.tw.awayday.citizensassist.UserMessages.OPENING_MAPS;
 
 public class CategorySelectionActivity extends AppCompatActivity {
 
@@ -61,8 +60,6 @@ public class CategorySelectionActivity extends AppCompatActivity {
         findViewById(R.id.street_lights).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String category = spinner.getSelectedItem().toString();
-                newIssue.addCategory(category);
                 newIssue.addCategory("Street Lights");
                 makeText(getApplicationContext(), OPENING_MAPS, LENGTH_SHORT).show();
                 startActivity(new Intent(CategorySelectionActivity.this, CaptureImageActivity.class));
