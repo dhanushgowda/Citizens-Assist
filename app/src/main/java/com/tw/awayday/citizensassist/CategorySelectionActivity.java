1e93d64c9600d8bde6d26431d791c986ff0a5839
Swathi Ashok <swathia@thoughtworks.com>
2016-09-14 17:06:25 +0530

    Swathi | Dramatically changes category selection page

--- app/src/main/java/com/tw/awayday/citizensassist/CategorySelectionActivity.java
+++ app/src/main/java/com/tw/awayday/citizensassist/CategorySelectionActivity.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import static android.widget.ArrayAdapter.createFromResource;
+import static android.widget.Toast.LENGTH_SHORT;
+import static android.widget.Toast.makeText;
 import static com.tw.awayday.citizensassist.MainActivity.newIssue;
+import static com.tw.awayday.citizensassist.UserMessages.*;
 
 public class CategorySelectionActivity extends AppCompatActivity {
 
@@ -17,18 +18,53 @@ public class CategorySelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_selection);
 
-        final Spinner spinner = (Spinner) findViewById(R.id.complaint_spinner);
-        int complaintCategories = R.array.complaintCategories;
-        int simpleSpinnerItem = android.R.layout.simple_spinner_item;
-        ArrayAdapter<CharSequence> adapter = createFromResource(CategorySelectionActivity.this, complaintCategories, simpleSpinnerItem);
-        adapter.setDropDownViewResource(R.layout.spinner_item);
-        spinner.setAdapter(adapter);
-
-        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.potholeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newIssue.addCategory("Potholes");
+                makeText(getApplicationContext(), OPENING_MAPS, LENGTH_SHORT).show();
+                startActivity(new Intent(CategorySelectionActivity.this, CaptureImageActivity.class));
+            }
+        });
+        findViewById(R.id.clogged_drains).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newIssue.addCategory("Clogged Drains");
+                makeText(getApplicationContext(), OPENING_MAPS, LENGTH_SHORT).show();
+                startActivity(new Intent(CategorySelectionActivity.this, CaptureImageActivity.class));
+            }
+        });
+        findViewById(R.id.bad_roads).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newIssue.addCategory("Bad Roads");
+                makeText(getApplicationContext(), OPENING_MAPS, LENGTH_SHORT).show();
+                startActivity(new Intent(CategorySelectionActivity.this, CaptureImageActivity.class));
+            }
+        });
+        findViewById(R.id.roadside_garbage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newIssue.addCategory("Roadside Garbage");
+                makeText(getApplicationContext(), OPENING_MAPS, LENGTH_SHORT).show();
+                startActivity(new Intent(CategorySelectionActivity.this, CaptureImageActivity.class));
+            }
+        });
+        findViewById(R.id.malfunctioningTrafficSignal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newIssue.addCategory("Traffic Signals");
+                makeText(getApplicationContext(), OPENING_MAPS, LENGTH_SHORT).show();
+                startActivity(new Intent(CategorySelectionActivity.this, CaptureImageActivity.class));
+            }
+        });
+        findViewById(R.id.street_lights).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String category = spinner.getSelectedItem().toString();
                 newIssue.addCategory(category);
+                newIssue.addCategory("Street Lights");
+                makeText(getApplicationContext(), OPENING_MAPS, LENGTH_SHORT).show();
                 startActivity(new Intent(CategorySelectionActivity.this, CaptureImageActivity.class));
             }
         });
