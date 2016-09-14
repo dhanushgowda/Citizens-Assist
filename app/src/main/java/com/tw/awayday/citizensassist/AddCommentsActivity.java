ce7554ba7cce10eba0a0dd57828bd1c9ca4b2581
Swathi Ashok <swathia@thoughtworks.com>
2016-09-14 12:21:12 +0530

    Swathi | Experimental fix for previous commit

--- app/src/main/java/com/tw/awayday/citizensassist/AddCommentsActivity.java
+++ app/src/main/java/com/tw/awayday/citizensassist/AddCommentsActivity.java
@@ -1,6 +1,5 @@
 package com.tw.awayday.citizensassist;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.Future;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
@@ -18,7 +18,6 @@ import static com.tw.awayday.citizensassist.ServerDetails.*;
 import static com.tw.awayday.citizensassist.TagLocationActivity.*;
 
 public class AddCommentsActivity extends AppCompatActivity {
-
     boolean issuedRaisedSuccessfully = false;
 
     @Override
@@ -30,34 +29,28 @@ public class AddCommentsActivity extends AppCompatActivity {
                 String userComments = ((EditText) findViewById(R.id.comments)).getText().toString();
                 newIssue.addComments(userComments);
 
-                Ion.with(getApplicationContext())
-                        .load(SERVER_URL + RAISE_ISSUE)
-                        .setJsonPojoBody(newIssue)
-                        .asJsonObject()
-                        .setCallback(new FutureCallback<JsonObject>() {
-                            @Override
-                            public void onCompleted(Exception e, JsonObject result) {
-                                if (e != null) {
-                                    issuedRaisedSuccessfully = false;
-                                    return;
-                                }
-
-                                if (result.get("issueStatus").getAsString().equals("Success")) {
-                                    issuedRaisedSuccessfully = true;
-                                    return;
-                                }
-
-                                issuedRaisedSuccessfully = false;
-                            }
-                        });
-
-                if (issuedRaisedSuccessfully) {
-                    startActivity(new Intent(AddCommentsActivity.this, RaiseIssueSuccessActivity.class));
-                } else {
-                    Toast.makeText(getApplicationContext(), "Sorry there was an error. Please try again!", Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(AddCommentsActivity.this, MainActivity.class));
-                }
+                saveIssueInServer();
             }
         });
+    }
+
+    private Future<JsonObject> saveIssueInServer() {
+        return Ion.with(getApplicationContext())
+                .load(SERVER_URL + RAISE_ISSUE)
+                .setJsonPojoBody(newIssue)
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonObject result) {
+                        issuedRaisedSuccessfully = e == null && result.get("issueStatus").getAsString().equals("Success");
+
+                        if (issuedRaisedSuccessfully) {
+                            startActivity(new Intent(AddCommentsActivity.this, RaiseIssueSuccessActivity.class));
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Sorry there was an error. Please try again!", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(AddCommentsActivity.this, MainActivity.class));
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/tw/awayday/citizensassist/Models/IssueContents.java
+++ app/src/main/java/com/tw/awayday/citizensassist/Models/IssueContents.java
@@ -3,13 +3,15 @@ package com.tw.awayday.citizensassist.Models;
 import com.google.android.gms.maps.model.LatLng;
 
 public class IssueContents {
-    private LatLng issueLocation;
     private String category;
     private String imagePath;
     private String userComments;
+    private String issueLatitude;
+    private String issueLongitude;
 
     public void addIssueLocation(LatLng issueLocation) {
-        this.issueLocation = issueLocation;
+        this.issueLatitude = String.valueOf(issueLocation.latitude);
+        this.issueLongitude = String.valueOf(issueLocation.longitude);
     }
 
     public void addCategory(String category) {
