0be3963aae317d5e9d4b8d963ddd06a3f5fb61e6
nayanab <nayanabm.94@gmail.com>
2016-09-15 16:45:58 +0530

    Nayana | I am committed

---
+++ app/src/main/java/com/tw/awayday/citizensassist/ViewComplaint.java
@@ -0,0 +1,22 @@
+package com.tw.awayday.citizensassist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ViewComplaint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_complaint);
+        findViewById(R.id.homeButton).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent mainActivity = new Intent(ViewComplaint.this, MainActivity.class);
+                mainActivity.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(mainActivity);
+            }
+        });
+    }
+}
