b404f850d0c21d797a29ee7328be17061c63ad2f
Swathi Ashok <swathia@thoughtworks.com>
2016-09-15 15:38:58 +0530

    Swathi | Adds an About Us page

--- app/src/main/java/com/tw/awayday/citizensassist/AboutUsActivity.java
+++ app/src/main/java/com/tw/awayday/citizensassist/AboutUsActivity.java
@@ -1,8 +1,13 @@
 package com.tw.awayday.citizensassist;
 
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -10,7 +15,24 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-        WebView webView = (WebView) findViewById(R.id.web_view);
-        webView.loadUrl("http://www.google.com");
+        String aboutUs = "Citizens-Assist\n\n" +
+                "Citizens Assist is an online portal for raising issues which allows citizens to " +
+                "report various problems in their city as well as allowing them to view the status " +
+                "of the problem. Any community issues can be raised with a few taps on your " +
+                "smartphone screen. You have the option to use GPS to mark the position of the issue" +
+                " or search for a location. Other functionalities include viewing issues that you " +
+                "raised, viewing all issues which can be sorted by type, area, severity, etc. " +
+                "Additionally, one can also look at trends based on the issues raised and resolved.\n"
+                + "\n\nTech Stack\n\nAndroid - Java\n\n" + "Node with MongoDB and Express\n\n";
+
+        TextView textView = (TextView) findViewById(R.id.about_us);
+        textView.setText(aboutUs);
+
+        findViewById(R.id.homeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AboutUsActivity.this, MainActivity.class));
+            }
+        });
     }
 }
